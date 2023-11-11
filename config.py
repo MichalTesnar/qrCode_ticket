91c83bdbe21f4a5daec32a803d6426cc04f455c8
Michal Tesnar <37748649+MichalTesnar@users.noreply.github.com>
2023-11-11 22:57:53 +0100

    Old python

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Change this file name to `config.py`
 
-INPUT_FILE_PATH = '../src/users.xlsx'
-OUTPUT_FILE_PATH = '../src/output.xlsx'
+INPUT_FILE_PATH = 'users.xlsx'
+OUTPUT_FILE_PATH = 'output.xlsx'
 ADMIN_PASSWORD = '1234'
-DOMAIN_NAME = 'https://qr-code-ticket-2.vercel.app/'
+DOMAIN_NAME = 'https://qr-code-ticket.vercel.app/'
 
-PAGE_TITLE = 'QR Ticket'
-EVENT_NAME = 'Your Event Name'
+PAGE_TITLE = 'QR Tickets SYSTEMS'
+EVENT_NAME = 'PLES'
