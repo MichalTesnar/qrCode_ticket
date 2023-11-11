c7899b96b7da8dbc0efbeec17eaaa027a6c8f3be
Michal Tesnar <37748649+MichalTesnar@users.noreply.github.com>
2023-11-11 22:47:55 +0100

    Another try

---
+++ config.py
@@ -0,0 +1,9 @@
+# Change this file name to `config.py`
+
+INPUT_FILE_PATH = '../src/users.xlsx'
+OUTPUT_FILE_PATH = '../src/output.xlsx'
+ADMIN_PASSWORD = '1234'
+DOMAIN_NAME = 'https://qr-code-ticket-2.vercel.app/'
+
+PAGE_TITLE = 'QR Ticket'
+EVENT_NAME = 'Your Event Name'
